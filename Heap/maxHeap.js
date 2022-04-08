a290a634ae6fae61ee686f33022162d0c129e28d
yunanah <bbyl6319@gmail.com>
2022-04-08 13:44:58 +0900

    ✨ Add Heap sample code

---
+++ Heap/maxHeap.js
@@ -0,0 +1,55 @@
+// 최대힙 (루트가 최대)
+
+class MaxHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  // 요소 추가
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex] < value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[currentIndex] = temp;
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  // 루트값 제거
+  pop() {
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      this.heap[currentIndex] < this.heap[leftIndex] ||
+      this.heap[currentIndex] < this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] < this.heap[rightIndex]) {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+        currentIndex = rightIndex;
+      } else {
+        const temp = this.heap[currentIndex];
+        this.heap[currentIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+        currentIndex = leftIndex;
+      }
+
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return returnValue;
+  }
+}

ea8552e6a5033079f9053efdb5a2a606ced4dd87
yunanah <bbyl6319@gmail.com>
2022-04-08 14:16:14 +0900

    ✨ add Trie sample code

---
+++ Trie/tireLinkedList.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        currentNode.children.set(char, new Node(currentNode.value + char));
+      }
+
+      currentNode = currentNode.children.get(char);
+    }
+  }
+
+  has(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        return false;
+      }
+
+      currentNode = currentNode.children.get(char);
+    }
+
+    return true;
+  }
+}
+
+const trie = new Trie();
+trie.insert("cat");
+trie.insert("can");
+console.log(trie.has("cat"));
+console.log(trie.has("can"));
+console.log(trie.has("cap"));

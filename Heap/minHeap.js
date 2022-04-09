463615d4ced5a2b6774f4d3b354ab2af4786466e
yunanah <bbyl6319@gmail.com>
2022-04-09 11:08:47 +0900

    ✨ add MinHeap sample code

---
+++ Heap/minHeap.js
@@ -0,0 +1,61 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (parentIndex !== 0 && this.heap[parentIndex].cost > value.cost) {
+      this._swap(parentIndex, currentIndex);
+
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) return;
+    if (this.heap.length === 2) return this.heap.pop();
+
+    const returnValue = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+    while (
+      (this.heap[leftIndex] &&
+        this.heap[currentIndex].cost > this.heap[leftIndex].cost) ||
+      (this.heap[rightIndex] &&
+        this.heap[currentIndex].cost > this.heap[rightIndex].cost)
+    ) {
+      if (this.heap[leftIndex] === undefined) {
+        // 왼쪽 정점이 없을 경우
+        this._swap(rightIndex, currentIndex);
+      } else if (this.heap[rightIndex] === undefined) {
+        // 오른쪽 정점이 없을 경우
+        this._swap(leftIndex, currentIndex);
+      } else if (this.heap[leftIndex].cost > this.heap[rightIndex].cost) {
+        this._swap(rightIndex, currentIndex);
+      } else if (this.heap[leftIndex].cost <= this.heap[rightIndex].cost) {
+        this._swap(leftIndex, currentIndex);
+      }
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return returnValue;
+  }
+
+  isEmpty() {
+    return this.heap.length === 1;
+  }
+
+  _swap(a, b) {
+    // 편의를 위해 배열의 요소를 swap하는 함수 작성
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+}

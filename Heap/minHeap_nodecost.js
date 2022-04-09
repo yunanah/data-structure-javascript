62127d09bd2c445d0036a64cfc80a95c842b2737
yunanah <bbyl6319@gmail.com>
2022-04-09 11:31:59 +0900

    💡 add comments

--- Heap/minHeap_nodecost.js
+++ Heap/minHeap_nodecost.js
@@ -8,17 +8,24 @@ class MinHeap {
     let currentIndex = this.heap.length - 1;
     let parentIndex = Math.floor(currentIndex / 2);
 
+    // `this.heap[parentIndex].cost`, `value.cost`를 통해
+    // 간선의 값으로 비교하도록 수정한다.
     while (parentIndex !== 0 && this.heap[parentIndex].cost > value.cost) {
-      this._swap(parentIndex, currentIndex);
+      this._swap(parentIndex, currentIndex); // 편의를 위해 별도로 _swap 함수를 구현했다.
 
       currentIndex = parentIndex;
       parentIndex = Math.floor(currentIndex / 2);
     }
   }
 
+  _swap(a, b) {
+    // 배열의 요소를 swap하는 함수 작성
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
   pop() {
-    if (this.isEmpty()) return;
-    if (this.heap.length === 2) return this.heap.pop();
+    if (this.isEmpty()) return; // 예외 로직
+    if (this.heap.length === 2) return this.heap.pop(); // 루트 정점만 남은 경우
 
     const returnValue = this.heap[1];
     this.heap[1] = this.heap.pop();
@@ -26,6 +33,9 @@ class MinHeap {
     let currentIndex = 1;
     let leftIndex = 2;
     let rightIndex = 3;
+
+    // 비교하는 부분에 전부 `.cost`를 붙여줍니다.
+    // 그리고 실제로 왼쪽, 오른쪽에 값이 있는지 체크하는 조건을 추가합니다.
     while (
       (this.heap[leftIndex] &&
         this.heap[currentIndex].cost > this.heap[leftIndex].cost) ||
